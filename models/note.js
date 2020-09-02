e00167837796cf5cadc29e3e88b3b39175723dc1
hanoelleb <hanoelleb@gmail.com>
2020-09-02 10:48:45 -0700

    basic models implemented

--- models/note.js
+++ models/note.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema (
+  {
+     user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+     topic: {type: String, required: true },
+     content: { type: String, required: true }
+  }
+);
+
+module.exports = mongoose.model('Note', NoteSchema);
--- models/user.js
+++ models/user.js
@@ -1,2 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var UserSchema = new Schema (
+  {
+     username: { type: String, required: true },
+     salted: { type: String, required: true }
+  }
+);
+
+module.exports = mongoose.model('User', UserSchema);
